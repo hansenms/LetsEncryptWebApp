0c2f3f3ea85b3a19188eca12448484526f8fb647
Michael Hansen <mihansen@microsoft.com>
2018-01-15 21:08:58 -0500

    Updated README and added some comments

--- CreateLetsEncryptWebApp.ps1
+++ CreateLetsEncryptWebApp.ps1
@@ -97,26 +97,31 @@ if (-not $(Get-ACMEVaultProfile -ListProfiles) -contains $VaultName)
     $vault = Get-ACMEVault -VaultProfile $VaultName
 }
 
+#Create new registration
 $reg = New-ACMERegistration -VaultProfile $VaultName -Contacts mailto:$ContactEmail -AcceptTos
 
 $alias = $Fqdn.replace('.','-')
 $alias = $alias + $(Get-Random).ToString()
 
+#Identifier for DNS
 New-ACMEIdentifier -VaultProfile $VaultName -Dns $Fqdn -Alias $alias
 
+#Start http challenge
 Complete-ACMEChallenge -VaultProfile $VaultName -IdentifierRef $alias -Force -ChallengeType http-01 -Handler manual -Regenerate -RepeatHandler
 $challenge = $(Update-ACMEIdentifier $alias -VaultProfile $VaultName -ChallengeType http-01).Challenges | Where-Object {$_.Type -eq "http-01"}
 
+#Figure out what the challenge file and content should be
 $challengeFile = $($challenge.HandlerHandleMessage -match "File Path:[^\[]+\[([^\]]+)\]" | Out-Null; $Matches[1])
 $fileComp = $challengeFile.Split("/")
 $challengeContent = $($challenge.HandlerHandleMessage -match "File Content:[^\[]+\[([^\]]+)\]" | Out-Null; $Matches[1])
 $challengeContent | Out-File -Encoding ASCII -NoNewline -FilePath ".\ACMEChallengeFile.txt"
 
+#Create the folder for the challenge file
 Create-WebAppDirectory -WebAppName $WebAppName -ResourceGroupName $ResourceGroupName -Directory $fileComp[0]
-
 $folder = $fileComp[0] + "/" + $fileComp[1]
 Create-WebAppDirectory -WebAppName $WebAppName -ResourceGroupName $ResourceGroupName -Directory $folder
 
+#Upload challenge file and web.config
 $cred = Copy-FileToWebApp -WebAppName $WebAppName -ResourceGroupName $ResourceGroupName -Destination $challengeFile -File ".\ACMEChallengeFile.txt"
 $fileloc = $fileComp[0] + "/" + $fileComp[1] + "/web.config"
 $cred = Copy-FileToWebApp -WebAppName $WebAppName -ResourceGroupName $ResourceGroupName -Destination $fileloc -File .\web.config -PublishingCredentials $cred
@@ -125,12 +130,13 @@ Start-Sleep -Seconds 10
 
 $challenge = $(Update-ACMEIdentifier $alias -VaultProfile $VaultName -ChallengeType http-01).Challenges | Where-Object {$_.Type -eq "http-01"}
 
+#Submit challenge responde
 if ($challenge.Status -ne 'valid') {
     Submit-ACMEChallenge -VaultProfile $VaultName -IdentifierRef $alias -ChallengeType http-01
 }
 
+#Check until valid
 $challenge = $(Update-ACMEIdentifier $alias -VaultProfile $VaultName -ChallengeType http-01).Challenges | Where-Object {$_.Type -eq "http-01"}
-
 $try = 0
 while (($challenge.Status -eq 'pending') -and ($try -lt 10)) {
     Write-Host "Sleeping while waiting for challenge validation..."
@@ -143,12 +149,13 @@ if ($challenge.Status -ne 'valid') {
     throw 'Failed to validate challenge'
 }
 
+#Generate cert
 $certName = $alias + "-cert"
 New-ACMECertificate $alias -VaultProfile $VaultName -Generate -Alias $certName
 Submit-ACMECertificate -CertificateRef $certName -VaultProfile $VaultName
 
+#Wait until cert is ready
 $cert = Update-ACMECertificate -CertificateRef $certName -VaultProfile $VaultName
-
 while ([String]::IsNullOrEmpty($cert.IssuerSerialNumber)) {
     Write-Host "Waiting for certficate...."
     Start-Sleep -Seconds 10
@@ -159,8 +166,10 @@ if ([String]::IsNullOrEmpty($CertificatePath)) {
     $CertificatePath = "C:\temp\" + $certName + ".pfx"
 }
 
+#Export PFX file
 Get-ACMECertificate $certName -ExportPkcs12 $CertificatePath -CertificatePassword (New-Object PSCredential "user",$CertificatePassword).GetNetworkCredential().Password -VaultProfile $VaultName
 
+#Bind the cert to the web app
 $binding = New-AzureRmWebAppSSLBinding `
     -WebAppName $WebAppName `
     -ResourceGroupName $ResourceGroupName `
@@ -168,6 +177,5 @@ $binding = New-AzureRmWebAppSSLBinding `
     -CertificateFilePath $CertificatePath `
     -CertificatePassword (New-Object PSCredential "user",$CertificatePassword).GetNetworkCredential().Password `
     -SslState SniEnabled
-
 
 Write-Host "All done."
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 Azure Web App with Let's Encrypt Certificate
 --------------------------------------------
+
+This repository contains example code for creating an [Azure Web App](https://azure.microsoft.com/en-us/services/app-service/web/) with a [Let's Encrypt](https://letsencrypt.org/) SSL Certificate. It uses the [ACMESharp](https://github.com/ebekker/ACMESharp) Powershell module. You can install the ACMESharp Module in Powershell with:
+
+```
+Install-Module ACMESharp -AllowClobber
+```
+
+All the code needed to set up a Web App, generate the certificate, and bind the certificate is contained in the [CreateLetsEncryptWebApp.ps1](CreateLetsEncryptWebApp.ps1) script. The script does the following:
+
+1. Creates a Web App with an App Service plan, if it doesn't exist already
+2. Pauses to allow the user to set a CNAME to point to the Web App. It is important to complete this step before continuing or the Web App will not allow the custom DNS name.
+3. Creates an ACME Vault and registration (if it doesn't exist).
+4. Generates a new ACME identifier for the DNS name.
+5. Starts a HTTP challenge.
+6. Uploads appropriate challenge reponse to the Web App.
+7. Submits the challenge. 
+8. Waits for challenge validation.
+9. Generates certificate.
+10. Binds certificate to the Web App
+
+If the Web App already exists, it will simple generate a new cert and bind it, effectively renewing the certificate. 
+
+To call the script:
+
+```
+.\CreateLetsEncryptWebApp.ps1 -ResourceGroupName "RESOURCE-GROUP-NAME" `
+-WebAppName "WEB-APP-NAME" -Fqdn "DOMAIN NAME" -Location "LOCATION" `
+-ContactEmail "EMAIL ADDRESS FOR REGISTRATION"
+```
+
